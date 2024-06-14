7d0f83c9ad84aee90327e442b52722518c7741d8
flintlocounloco <88441705+flintlocounloco@users.noreply.github.com>
2024-06-14 20:59:36 +0000

    Mundo guardado 2024-06-14

--- servidor.py
+++ servidor.py
@@ -10,8 +10,8 @@
 
 # Ngrok region
 # Code           Place
-#-----------     ---------------------------
-# ap	          Asia/Pacific (Singapore)
+#--------------------------------------
+# ap	      Asia/Pacific (Singapore)
 # au		  Australia (Sydney)
 # eu		  Europe (Frankfurt)
 # in		  India (Mumbai)
